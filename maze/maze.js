f624f0dcd9d99a0a8beaaf947ff37810da836a7a
aaay-aaay <aaayaaay58@gmail.com>
2018-10-27 12:25:21 +0100

    sogwimnoinroin

--- maze/maze.js
+++ maze/maze.js
@@ -13,12 +13,11 @@ request.onreadystatechange = function() {
     rowLength = maze[0].length;
     columnLength = maze.length;
     mazeElement = document.getElementById('maze');
-    for (i = 0; i < columnLength; i++) {
+    for (i = 0; i < rowLength; i++) {
         mazeElement.appendChild(tr = document.createElement('tr'));
-        for (j = 0; j < rowLength; j++) {
+        for (j = 0; j < columnLength; j++) {
             tr.appendChild(td = document.createElement('td'));
-            console.log(images[maze[i][j]]);
-            td.appendChild(images[maze[i][j]]);
+            td.appendChild(images[maze[j][i]]);
         }
     }
 };
